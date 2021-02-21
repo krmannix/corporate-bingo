49da991dd91727ce6b5a2904f8218b4b731f89e3
K. Rodman Mannix <kevinrmannix@gmail.com>
2021-02-21 13:06:16 -0500

    Board

--- src/Board.tsx
+++ src/Board.tsx
@@ -3,53 +3,38 @@ import { Block, BlockProps } from './Block'
 import './Board.css'
 
 interface BoardProps {
-  words: string[],
-}
-
-interface BoardState {
   blocks: BlockProps[],
+  isWinner: boolean,
+  reset: () => void,
 }
 
-const freeBlockProps = {
-  isClicked: true,
-  isFreeBlock: true,
-  word: 'Free',
-}
-
-class Board extends React.Component<BoardProps, BoardState> {
-  constructor(props: BoardProps) {
-    super(props)
-    this.state = {
-      blocks: props.words.map(word => ({ isClicked: false, isFreeBlock: false, word }))
+export function Board(props: BoardProps) {
+  const sqrt = Math.sqrt(props.blocks.length)
+  const blockGrid = []
+  let i, j;
+  for (i = 0; i < sqrt; i++) {
+    let blockRow = []
+    for (j = 0; j < sqrt; j++) {
+      blockRow.push(props.blocks[i * sqrt + j])
     }
-    this.buildBlocks = this.buildBlocks.bind(this)
+    blockGrid.push(blockRow)
   }
 
-  render() {
-    const blocks = this.buildBlocks().map(blockRow => [...blockRow.map(block => <Block {...block} />), <br />])
-
-    return <div className='board'>
-      {blocks}
+  const winnersCircle = props.isWinner ? <div>Bingo!</div> : null
+  const blocks = blockGrid.map((blockRow, index) => {
+    return <div key={index}>
+      {blockRow.map(block => <Block key={block.word} {...block} />)}
     </div>
-  }
+  })
+  const resetButton = props.isWinner ?
+    <button onClick={props.reset}>Play Again</button> :
+    null
 
-  buildBlocks() {
-    const blocks = this.state.blocks.map(blockProps => blockProps)
-    blocks.splice(this.state.blocks.length / 2, 0, freeBlockProps)
-
-    const sqrt = Math.sqrt(blocks.length)
-    const blocks2d = []
-    let i, j;
-    for (i = 0; i < sqrt; i++) {
-      let blockRow = []
-      for (j = 0; j < sqrt; j++) {
-        blockRow.push(blocks[i * sqrt + j])
-      }
-      blocks2d.push(blockRow)
-    }
-
-    return blocks2d
-  }
+  return <div>
+    {winnersCircle}
+    <div className='board'>{blocks}</div>
+    {resetButton}
+  </div>
 }
 
 export default Board
